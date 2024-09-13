2486acf514050b701843f47dcd8f1eea4f5ac695
naimuddin94 <developernaimuddin@gmail.com>
2024-09-13 10:10:55 +0600

    update page.tsx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,14 +1,17 @@
 import ComponentOne from "@/components/ComponentOne";
 import ComponentTwo from "@/components/ComponentTwo";
 import CustomLoading from "@/components/CustomLoading";
+import ErrorBoundary from "@/components/ErrorBoundary";
 import { Suspense } from "react";
 
 export default function BlogPage() {
   return (
     <div className="flex justify-center gap-10">
-      <Suspense fallback={<CustomLoading />}>
-        <ComponentOne />
-      </Suspense>
+      <ErrorBoundary fallback={<h1>Something went wrong from error boundary.</h1>}>
+        <Suspense fallback={<CustomLoading />}>
+          <ComponentOne />
+        </Suspense>
+      </ErrorBoundary>
       <ComponentTwo />
     </div>
   );
